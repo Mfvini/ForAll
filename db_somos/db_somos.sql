068afb6eb3296168c0c444bdda0ad86a7fc27e88
Vinicius F. Murcia <vini.fernandes.murcia@gmail.com>
2026-05-31 16:01:56 -0300

    feat: Adição do banco db_somos completo.

--- db_somos/db_somos.sql
+++ db_somos/db_somos.sql
@@ -1,1 +1,139 @@
-USE db_somos;
+CREATE DATABASE  IF NOT EXISTS `db_somos` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_somos`;
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: db_somos
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pacientes`
+--
+
+DROP TABLE IF EXISTS `pacientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pacientes` (
+  `id` int NOT NULL,
+  `necessita_doacao` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `pacientes_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pacientes`
+--
+
+LOCK TABLES `pacientes` WRITE;
+/*!40000 ALTER TABLE `pacientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pacientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pontos_doacao`
+--
+
+DROP TABLE IF EXISTS `pontos_doacao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pontos_doacao` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `endereco` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bairro` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cidade` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefone` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `itens_necessarios` text COLLATE utf8mb4_unicode_ci,
+  `criado_em` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pontos_doacao`
+--
+
+LOCK TABLES `pontos_doacao` WRITE;
+/*!40000 ALTER TABLE `pontos_doacao` DISABLE KEYS */;
+INSERT INTO `pontos_doacao` VALUES (1,'Centro de Arrecadação Solidária','Rua das Flores, 123','Centro','São Paulo','(11) 99999-1111','Arroz, feijão, óleo e leite em pó.','2026-05-27 18:34:24'),(2,'Banco de Alimentos Comunitário','Av. Paulista, 1500','Bela Vista','São Paulo','(11) 99999-2222','Cesta básica completa e agasalhos.','2026-05-27 18:34:24');
+/*!40000 ALTER TABLE `pontos_doacao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `psicologos`
+--
+
+DROP TABLE IF EXISTS `psicologos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `psicologos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `crp` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefone` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `disponibilidade` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `psicologos`
+--
+
+LOCK TABLES `psicologos` WRITE;
+/*!40000 ALTER TABLE `psicologos` DISABLE KEYS */;
+INSERT INTO `psicologos` VALUES (1,'Dra. Ana Beatriz','CRP-06/123456','(11) 98888-3333','Segunda a Sexta, das 18h às 21h');
+/*!40000 ALTER TABLE `psicologos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tipo` enum('diretor','psicologo','paciente') NOT NULL,
+  `criado_em` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (3,'Teste Novo','teste@email.com','$2y$10$9ug1SrKtrKgBUc33NYsDXuFG9WzkUgh7LTrNkTUyzRBB4E3.R.CIu','paciente','2026-05-25 19:53:45'),(4,'Vinicius Fernandes','vini@email.com','$2y$10$pi/W4/9QbYIvG1hOwP0fju59ZF93bLBCh2GtFHbzFkF0AYEBJlDya','diretor','2026-05-25 22:01:01'),(6,'Vinícius Fernandes','vinii@email.com','$2y$10$jBif5du4Q5IXUbFI0dlIwuEsJmCPN.ZEmnglBeuoegzPvGRRNA31m','paciente','2026-05-27 20:26:04'),(8,'Vinícius Fernandes','vini.teste.psi@email.com','$2y$10$39z3Pxx9xYPMoPIc/kBXuuqjRLe4.1ih8RKOae33u6KVaBxsb0vFi','psicologo','2026-05-28 18:28:12');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-31 16:01:07
